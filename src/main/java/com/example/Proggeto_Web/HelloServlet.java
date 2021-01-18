ce4fb8ded1a7e74baed3f18636ae6a76865b8445
Jorge <zgzserrano@gmail.com>
2021-01-18 19:52:16 +0100

    login and table added and starting with vue.js

--- src/main/java/com/example/Proggeto_Web/HelloServlet.java
+++ src/main/java/com/example/Proggeto_Web/HelloServlet.java
@@ -13,8 +13,8 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        super.init();
-        DAO.registerDriver();
+        //super.init();
+        //DAO.registerDriver();
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
